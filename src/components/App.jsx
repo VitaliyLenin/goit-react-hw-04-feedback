98d49b36e122e760340f66d3c9eadef786d2f2de
VitaliyLenin <vitaliylenin77@gmail.com>
2023-02-09 15:34:11 +0200

    Refactoring code to React hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,57 @@
-import React from 'react';
+import { useState } from 'react';
 
 import Statistics from './Statistics/statistics';
 import FeedbackOptions from './Feedbackoption/feedbackoption';
 import Section from './Section/Section';
 import Notification from './Notification/notification';
 
-class App extends React.Component {
-  state = {
+export default function App() {
+  const [feedBack, setFeeback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
-
-  countPositiveFeedbackPercentage() {
-    const total = this.countTotalFeedback();
-    if (total === 0) {
-      return 0;
-    }
-    return Math.floor((this.state.good / total) * 100);
-  }
-
-  handleLeaveFeedback = option => {
-    this.setState(prevState => {
+  const leaveFeedback = option => {
+    setFeeback(prevState => {
       return { ...prevState, [option]: prevState[option] + 1 };
     });
   };
 
-  render() {
-    const total = this.countTotalFeedback();
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = feedBack;
 
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handleLeaveFeedback}
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = feedBack;
+    if (good === 0) {
+      return 0;
+    }
+    return Math.floor((good * 100) / countTotalFeedback());
+  };
+
+  const options = Object.keys(feedBack);
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={leaveFeedback} />
+      </Section>
+      {total === 0 ? (
+        <Notification message="There is no feedback :(" />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            options={feedBack}
+            total={total}
+            positivePercentage={positivePercentage}
           />
         </Section>
-        {total === 0 ? (
-          <Notification message="There is no feedback :(" />
-        ) : (
-          <Section title="Statistics">
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={total}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          </Section>
-        )}
-      </>
-    );
-  }
+      )}
+    </>
+  );
 }
-
-export { App };
--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import css from './statistics.module.css';
 
-const Statistics = props => (
+const Statistics = ({ options, total, positivePercentage }) => (
   <>
-    <p className={css.stat_text}>Good: {props.good}</p>
-    <p className={css.stat_text}>Neutral: {props.neutral}</p>
-    <p className={css.stat_text}>Bad: {props.bad}</p>
-    <p className={css.stat_text}>Total : {props.total}</p>
-    <p className={css.stat_text}>
-      Positive feedback : {props.positivePercentage}%
-    </p>
+    <p className={css.stat_text}>Good: {options.good}</p>
+    <p className={css.stat_text}>Neutral: {options.neutral}</p>
+    <p className={css.stat_text}>Bad: {options.bad}</p>
+    <p className={css.stat_text}>Total : {total}</p>
+    <p className={css.stat_text}>Positive feedback : {positivePercentage}%</p>
   </>
 );
 
 Statistics.propTypes = {
-  good: PropTypes.number.isRequired,
-  neutral: PropTypes.number.isRequired,
-  bad: PropTypes.number.isRequired,
+  options: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+  }),
   total: PropTypes.number.isRequired,
   positivePercentage: PropTypes.number.isRequired,
 };
